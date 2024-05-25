eed8c696e65d97754d3519951c80d7b535cf34ec
tomasshiao <tomasshiao2001@gmail.com>
2024-05-25 18:50:25 -0300

    feat: Add Sprite struct with vector and tuple constructors

--- Client/sprite.h
+++ Client/sprite.h
@@ -1,6 +1,10 @@
 #ifndef SPRITE_H
 #define SPRITE_H
 
+#include <vector>
+#include <tuple> 
+#include <stdexcept>
+
 struct Sprite {
     int x;
     int y;
@@ -9,6 +13,22 @@ struct Sprite {
 
     Sprite(int x, int y, int width, int height)
         : x(x), y(y), width(width), height(height) {}
+
+    explicit Sprite(const std::vector<int>& values) {
+        if (values.size() != 4) {
+            throw std::invalid_argument("Vector must contain exactly four elements");
+        }
+        x = values[0];
+        y = values[1];
+        width = values[2];
+        height = values[3];
+    }
+
+    explicit Sprite(const std::tuple<int, int, int, int>& values)
+        : x(std::get<0>(values)),
+        y(std::get<1>(values)),
+        width(std::get<2>(values)),
+        height(std::get<3>(values)) {}
 };
 
 #endif  // SPRITE_H
