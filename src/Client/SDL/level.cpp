225ee198fedabfb97c9912660fffbcb05c240e11
LucasOshiro1 <oshirolucas26@gmail.com>
2024-06-17 14:51:44 -0300

    temporary fix colonius floor

--- src/Client/SDL/level.cpp
+++ src/Client/SDL/level.cpp
@@ -70,8 +70,9 @@ Level::Level(int level) {
                 this->paths[tiles_types[i]] = tilesSprites[i];
                 this->pixels_pos[tiles_types[i]] = beachSprites[i];
             }
-            this->max_pixel_x_floor = 4525;
-            this->floor_height = 200;
+            this->max_pixel_x_floor = 2491;
+            this->floor_height = 70;
+
             this->background_width = 2000;
             this->background_height = 839;
             break;
@@ -168,7 +169,7 @@ void Level::draw_floor(SDL2pp::Window& window, SDL2pp::Renderer& renderer, SDL2p
 
     renderer.Copy(floor, SDL2pp::Rect(this->pixels_pos[TileType::FLOOR][index_x], this->pixels_pos[TileType::FLOOR][index_y], this->pixels_pos[TileType::FLOOR][index_width], this->pixels_pos[TileType::FLOOR][index_height]), 
                         SDL2pp::Rect(0, window.GetHeight() - this->floor_height, window.GetWidth(), this->floor_height));
-    this->pixels_pos[TileType::FLOOR][index_x] += 50;
+    this->pixels_pos[TileType::FLOOR][index_x] += player_speed;
 }
 
 
