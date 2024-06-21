c334ebc2f80e51d9a6c694573164ef3b47688583
LucasOshiro1 <oshirolucas26@gmail.com>
2024-06-21 15:05:17 -0300

    Modified camera movement

--- src/Client/SDL/projectile.h
+++ src/Client/SDL/projectile.h
@@ -4,6 +4,7 @@
 #include <SDL2pp/SDL2pp.hh>
 #include "../../Common/sprite.h"
 #include "../../Common/DTO/bullet.h"
+#include "../../Common/DTO/player.h"
 
 #include <map>
 #include <list>
@@ -32,6 +33,6 @@ public:
 
 	std::list<RectangularSprite>::iterator img_coords(uint32_t bulletId);
 	
-	void draw_projectile(SDL2pp::Window& window, SDL2pp::Renderer& renderer, std::unique_ptr<SDL2pp::Texture>& projectile, std::vector<BulletDTO>& bullets);
+	void draw_projectile(SDL2pp::Window& window, SDL2pp::Renderer& renderer, std::unique_ptr<SDL2pp::Texture>& projectile, std::vector<BulletDTO>& bullets, PlayerDTO& player, int dir_x_screen, int dir_y_screen);
 };
 #endif
