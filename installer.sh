017c0bee8cc1fc3ad7461dbcdc60472e36487dca
tomasshiao <tomasshiao2001@gmail.com>
2024-06-23 21:34:27 -0300

    Update Vagrantfile and Installer

--- installer.sh
+++ installer.sh
@@ -6,20 +6,26 @@ set -e
 # Update and install dependencies
 sudo apt-get update
 sudo apt-get install -y build-essential cmake git libyaml-cpp-dev libfmt-dev
-sudo apt-get install -y qtbase5-dev qtdeclarative5-dev qt5-qmake qttools5-dev-tools libqt5widgets5 libqt5core5a
-sudo apt-get install -y libgtest-dev
+sudo apt-get install -y libgl1-mesa-dev libglu1-mesa-dev libglew-dev
 sudo apt-get install -y libjpeg-dev libpng-dev libfreetype-dev libopusfile-dev libflac-dev libxmp-dev libfluidsynth-dev libwavpack-dev cmake libmodplug-dev
-# sudo apt-get install -y libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev libsdl2-gfx-dev
+sudo apt-get install -y libgl-dev libegl-dev libfontconfig1-dev libinput-dev libfontconfig1-dev libfreetype6-dev libx11-dev libx11-xcb-dev libxext-dev libxfixes-dev libxi-dev libxrender-dev libxcb1-dev libxcb-cursor-dev libxcb-glx0-dev libxcb-keysyms1-dev libxcb-image0-dev libxcb-shm0-dev libxcb-icccm4-dev libxcb-sync-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-randr0-dev libxcb-render-util0-dev libxcb-util-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev
 
-# Instalación de SDL2
-wget https://github.com/libsdl-org/SDL/releases/download/release-2.30.4/SDL2-2.30.4.tar.gz
-tar -xvf SDL2-2.30.4.tar.gz 
-cd SDL2-2.30.4
-mkdir build
-cd build
-../configure --prefix=/usr/local/sdl2
-make -j2
-sudo make install
+# Install Qt5
+sudo apt-get install -y qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
+sudo apt-get install -y libqt5widgets5 libqt5gui5 libqt5dbus5 libqt5network5 libqt5core5a
+
+# Install SDL2 and its extensions from Ubuntu repositories
+sudo apt-get install -y libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev
+
+# Install and setup GTest
+sudo apt-get install -y libgtest-dev libgmock-dev
+cd /usr/src/googletest
+sudo cmake .
+sudo make
+sudo cp lib/*.a /usr/lib
+
+# Update library cache
+sudo ldconfig
 
 # Remove old files if previously installed
 if [ -d "./build" ]; then
