03c5a5f6f4a050dcd4056b5b1402b4c9eea18009
franciscoduc4 <fduca@fi.uba.ar>
2024-06-25 11:33:18 -0300

    fixed some comments

--- src/Client/client.cpp
+++ src/Client/client.cpp
@@ -30,9 +30,9 @@ void Client::start() {
     bool clientJoinedGame = false;
     do {
         // LobbyInit init;
-        // std::pair<bool, LobbyMessage> qtResult = init.launchQT(this->lobbyController, // (bool&)clientJoinedGame);
-        clientJoinedGame = qtResult.first;
-        if (!clientJoinedGame) {
+        // // std::pair<bool, LobbyMessage> qtResult = init.launchQT(this->lobbyController, // (bool&)clientJoinedGame);
+        // clientJoinedGame = qtResult.first;
+        // if (!clientJoinedGame) {
         //       return;
         // }
         // TODO: Continue with SDL.
@@ -77,7 +77,7 @@ void Client::start() {
         } else {
             std::cerr << "Failed to receive response for create game." << std::endl;
             return;
-        }*/
+        }
 
         // END - TESTING SKIP QT
         //CharacterType pj = qtResult.second.getCharacter();
