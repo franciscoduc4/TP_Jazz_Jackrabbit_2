b1945bdc58206f903e535a84577ab289e3bb56b2
tomasshiao <tomasshiao2001@gmail.com>
2024-06-26 09:49:22 -0300

    move gmock library files into project

--- Tests/ProtocolTests/ServerProtocolTest.cpp
+++ Tests/ProtocolTests/ServerProtocolTest.cpp
@@ -3,13 +3,16 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include "../../src/Server/Protocol/deserializer.h"
 #include "../../src/Server/Protocol/serializer.h"
+
 #include "socket.h"
 
 class MockSocket : public Socket {
 public:
-    MockSocket(const std::string& port) : Socket(port.c_str()) {}
+    explicit MockSocket(const std::string& port) : Socket(port.c_str()) {}
     MOCK_METHOD(int, sendall, (const void* msg, unsigned int len, bool* wasClosed), (override));
+    MOCK_METHOD(int, recvall, (void* msg, unsigned int len, bool* wasClosed), (override));
 };
 
 class SerializerTest : public ::testing::Test {
@@ -75,4 +78,82 @@ TEST_F(SerializerTest, SendGameDTOSendsCorrectData) {
             .Times(::testing::AtLeast(1));
 
     serializer.sendGameDTO(gameDto);
+}
+
+
+class DeserializerTest : public ::testing::Test {
+protected:
+    std::shared_ptr<MockSocket> mockSocket;
+    std::atomic<bool> keepPlaying;
+    std::atomic<bool> inGame;
+    Deserializer deserializer;
+
+    DeserializerTest() :
+            mockSocket(std::make_shared<MockSocket>("8080")),
+            keepPlaying(true),
+            inGame(true),
+            deserializer(mockSocket, keepPlaying, inGame){}
+};
+
+TEST_F(DeserializerTest, DeserializeCreateGameReturnsCorrectDTO) {
+    uint8_t playerId = 1;
+    uint8_t mapId = 2;
+    uint8_t maxPlayers = 3;
+    CharacterType characterType = CharacterType::JAZZ;
+    std::string gameName = "TestGame";
+    std::vector<char> nameBuffer(gameName.begin(), gameName.end());
+
+    EXPECT_CALL(*mockSocket, recvall(testing::_, testing::_, testing::_))
+            .Times(6)
+            .WillOnce(testing::Invoke([&](void* buffer, size_t len, bool* wasClosed) {
+                *(static_cast<char*>(buffer)) = static_cast<char>(Command::CREATE_GAME);
+                return len;
+            }))
+            .WillOnce(testing::Invoke([&](void* buffer, size_t len, bool* wasClosed) {
+                *(static_cast<uint8_t*>(buffer)) = mapId;
+                return len;
+            }))
+            .WillOnce(testing::Invoke([&](void* buffer, size_t len, bool* wasClosed) {
+                *(static_cast<uint8_t*>(buffer)) = maxPlayers;
+                return len;
+            }))
+            .WillOnce(testing::Invoke([&](void* buffer, size_t len, bool* wasClosed) {
+                *(static_cast<CharacterType*>(buffer)) = characterType;
+                return len;
+            }))
+            .WillOnce(testing::Invoke([&](void* buffer, size_t len, bool* wasClosed) {
+                *(static_cast<uint8_t*>(buffer)) = gameName.size();
+                return len;
+            }))
+            .WillOnce(testing::Invoke([&](void* buffer, size_t len, bool* wasClosed) {
+                std::copy(nameBuffer.begin(), nameBuffer.end(), static_cast<char*>(buffer));
+                return len;
+            }));
+
+    auto dto = deserializer.getCommand(playerId);
+
+    ASSERT_NE(dto, nullptr);
+
+    auto createGameDto = dynamic_cast<CreateGameDTO*>(dto.get());
+
+    EXPECT_EQ(createGameDto->getPlayerId(), playerId);
+    EXPECT_EQ(createGameDto->getMapId(), mapId);
+    EXPECT_EQ(createGameDto->getMaxPlayers(), maxPlayers);
+    EXPECT_EQ(createGameDto->getCharacterType(), characterType);
+    EXPECT_EQ(createGameDto->getGameName(), gameName);
+}
+
+TEST_F(DeserializerTest, DeserializeCreateGameReturnsNullOnSocketClose) {
+    uint8_t playerId = 1;
+
+    EXPECT_CALL(*mockSocket, recvall(testing::_, testing::_, testing::_))
+            .WillOnce(testing::Invoke([](void* buffer, size_t len, bool* wasClosed) {
+                *wasClosed = true;
+                return 0;
+            }));
+
+    auto dto = deserializer.getCommand(playerId);
+
+    EXPECT_NE(dto, nullptr);
+    EXPECT_EQ(dto->getCommand(), Command::IDLE);
 }
